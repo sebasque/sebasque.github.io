b8b11e527b608dbd11adaf02d0265176a937f3a0
Michael <michael@Michaels-MacBook-Pro.local>
2017-12-20 12:12:58 -0500

    Framework established.

--- site.js
+++ site.js
@@ -1,11 +1,41 @@
 
+/* JS File for Personal Website */
+
 $(document).ready(function() {
-    $("#content").css({
-        "margin-top": "150px",
+    $("main").css({
+        "margin-top": "200px",
         "opacity": "0"
     });
-    $("#content").animate({
+    $("main").animate({
         "margin-top": "40px",
         "opacity": "1"
     }, "slow");
+
+    $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+
+          // Store hash
+          var hash = this.hash;
+
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+    });
+});
+
+$("aboutbtn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#about").offset().top},
+        'slow');
 });
